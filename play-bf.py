735ddb4e1b69bbeea48ec1718969e7ddfe90b405
PenguinPush <icyzminecraft@gmail.com>
2024-05-03 14:43:40 -0400

    brainfuck interpreter

---
+++ play-bf.py
@@ -0,0 +1,13 @@
+import brainfuck
+import numpy
+import time
+
+fps = 30
+video_frames_x4 = numpy.load("assets/video_frames_x4.npy")
+
+for i in range(video_frames_x4.shape[0]):
+    with open(f"bf/{i}.txt", "r") as file:
+        # print(file.read())
+        brainfuck.evaluate(file.read())
+        time.sleep(1/fps)
+
